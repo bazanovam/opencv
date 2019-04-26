d37445485e179f6aeb0f0ca6fc2fb82198bb3136
unknown <bazanovam@gmail.com>
2019-04-26 18:02:34 +0300

    sdas

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 #create an instance of Flask
 app = Flask(__name__)
@@ -10,7 +10,7 @@ port = int(os.environ.get('PORT', 3000))
 #execute hello function when page URL is loaded
 @app.route('/')
 def hello():
-    return "Hello Python World"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=port) 
