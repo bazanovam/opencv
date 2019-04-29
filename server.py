71d586b65b3d6dab47abde85a22c14bbe0ec0185
unknown <bazanovam@gmail.com>
2019-04-29 13:15:18 +0300

    asd

--- server.py
+++ server.py
@@ -1,16 +1,32 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
+from cfenv import AppEnv
+from hdbcli import dbapi
 
-#create an instance of Flask
-app = Flask(__name__)
+app = Flask(__name__)		
+env = AppEnv()		
 
-#assign the port that the flask application runs on
 port = int(os.environ.get('PORT', 3000))
+hana = env.get_service(name='hdi-db')	
 
-#execute hello function when page URL is loaded
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    #connect to DB using credentials from hdi-db service
+    conn = dbapi.connect(address=hana.credentials['host'],
+                         port= int(hana.credentials['port']),
+                         user = hana.credentials['user'],
+                         password = hana.credentials['password'],
+                         CURRENTSCHEMA=hana.credentials['schema'])
+    cursor = conn.cursor()
+
+    #execute SQL query
+    cursor.execute("select CURRENT_UTCTIMESTAMP from DUMMY", {})      
+    ro = cursor.fetchone()        #get the first result
+    cursor.close()
+    conn.close()        #close DB connection
+
+    #return query results
+    return "Current time is: " + str(ro["CURRENT_UTCTIMESTAMP"])  
 
 if __name__ == '__main__':
-    app.run(port=port) 
+    app.run(port=port)
