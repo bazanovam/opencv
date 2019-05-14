13b5d572c5ea0bebd25805bf5a56b904325fceb3
unknown <bazanovam@gmail.com>
2019-05-14 17:16:33 +0300

    a

---
+++ app.py
@@ -0,0 +1,24 @@
+import cv2
+import time
+
+angle = 1
+while 1:
+    # read image as grey scale
+    img = cv2.imread('1.jpg')
+    # get image height, width
+    (h, w) = img.shape[:2]
+    # calculate the center of the image
+    center = (w / 2, h / 2)
+    
+    scale = 1.0
+    
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(img, M, (h, w))
+    writeStatus = cv2.imwrite("1.jpg", rotated)
+    if writeStatus is True:
+        print("image written")
+    else:
+        print("problem") # or raise exception, handle problem, etc.
+
+    angle += 2
+    time.sleep(2)
